07034f98421903d5ab5038c1ff958bb33d93c670
Andres27Milton <julioblancomiltonenrique@gmail.com>
2025-06-17 15:15:49 -0500

    feat Se crea proceso 1 asociado al menu

---
+++ src/main/java/org/example/jhon.java
@@ -0,0 +1,30 @@
+package org.example;
+import java.util.Scanner;
+public class jhon {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //entrada
+        Scanner KeyEntry= new Scanner(System.in);
+        Integer menuOption;
+
+        //prceso 1: crear menu de opciones
+        System.out.println("\n*************");
+        System.out.println("\nQue quieres resalizar?");
+        System.out.println("1. Guardar un prenda en BD");
+        System.out.println("2. Mostrar el inventario de prendas");
+        System.out.println("3. modificar los datos de una prenda.");
+        System.out.println("4. Eliminar una prenda del sistema");
+        System.out.println("3. modificar los datos de una prenda.");
+
+
+        //proceso 2
+        //proceso 3
+
+
+
+
+
+
+    }
+}
+}
